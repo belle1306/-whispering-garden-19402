1b197e448e99c1cf1a8044cbec3e08e91705da14
belle1306 <belle1306@gmail.com>
2021-01-19 05:51:36 +0800

    start

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let cors = require('cors');
+// let createError = require('http-errors');
+// let express = require('express');
+// let path = require('path');
+// let cookieParser = require('cookie-parser');
+// let logger = require('morgan');
+// let cors = require('cors');
 
-let app = express();
+// let app = express();
 
-let apiRouter = require('./routes/api');
+// let apiRouter = require('./routes/api');
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cors());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// // app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.get('/', (req, res, next) => {
-  res.send('Access the API at path /api');
+// app.get('/', (req, res, next) => {
+//   res.send('Access the API at path /api');
+// });
+
+// app.use('/api', apiRouter);
+
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-app.use('/api', apiRouter);
-
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
 
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
+
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.send('error');
+// });
 
 module.exports = app;
